bad1fe226df0a8cba22ccd27720474215d852670
Jin Pae <jin@jinpae.com>
2015-08-23 18:19:56 +0900

    Add nav overlay and change hero bg

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	/**
-	 * Waypoints
+	 * Waypoints.
 	 */
 	$('.wp1').waypoint(function() {
 		$('.wp1').addClass('animated fadeInLeft');
@@ -30,5 +30,64 @@ $(document).ready(function() {
 		$('.wp5').addClass('animated fadeInUp');
 	}, {
 		offset: '75%'
+	});
+
+	/**
+	 * Navigation Overlay.
+	 */
+	$('.navbar-toggle').click(function() {
+		$(this).toggleClass('active');
+		$('.navigation').toggleClass('open');
+		event.preventDefault();
+	});
+
+	$('.overlay ul li a').click(function() {
+		$('.navbar-toggle').toggleClass('active');
+		$('.navigation').toggleClass('open');
+	});
+
+	$('.overlay').click(function() {
+		$('.navbar-toggle').toggleClass('active');
+		$('.navigation').toggleClass('open');
+	});
+
+	/**
+	 * Smooth Scrolling.
+	 */
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1400);
+				return false;
+			}
+		}
+	});
+
+	$('.top').click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+			scrollTop: 0
+		}, 700);
+		return false;
+	});
+
+	/**
+	 * Fixed Header BG Toggle.
+	 */
+	$(function() {
+		$(window).scroll(function() {
+			var scroll = $(window).scrollTop();
+
+			if (scroll >= 20) {
+				$('.fixed-navbar').addClass('navbar-with-bg');
+			} else {
+				$('.fixed-navbar').removeClass('navbar-with-bg');
+			}
+		});
 	});
 });
